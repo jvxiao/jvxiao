e9898ec0e2c87dcd29926d2be5f14bed06f8c298
jvxiao <2805431021@qq.com>
2025-06-06 23:06:47 +0800

    feat:  获取博客列表

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ I am a full-time front-end developer, and in the past few years, I have mainly u
 In my spare time, I enjoy learning new things and creating some very interesting projects. If you like my projects, please don't hesitate to give me a star or kindly consider [sponsoring me by afdian](https://afdian.com/a/jvxiao).
 
 Feel free to contact me.
+
+## [Latest blog posts](https://jvxiao.cn)
+<!-- blog-start-->
+- 2025-05-27 [搭建个人博客系列--(2) 动手搭建自己的第一个博客站点](https://www.jvxiao.cn/posts/build-personal-blog2.html)
+- 2025-05-25 [搭建个人博客系列--(1) 为什么每个人都该有个数字自留地](https://www.jvxiao.cn/posts/build-personal-blog-1.html)
+- 2025-05-23 [Javascript中两种不同的继承方式对比](https://www.jvxiao.cn/posts/Differences-between-two-kinds-inherit-approches.html)
+- 2025-05-23 [关于微信云托管对象存储那点事](https://www.jvxiao.cn/posts/Manage-wx-could-service.html)
+- 2025-05-23 [糟糕，Vite proxy error ECONNREFUSED](https://www.jvxiao.cn/posts/Vite-server-error.html)
+- [阅读更多...]('https://www.jvxiao.cn/archives/')
+<!-- blog-end -->
---
+++ src/updatePostList.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { readFileSync, writeFileSync } from 'fs';
+
+const JSON_URL = 'https://raw.githubusercontent.com/jvxiao/jvxiao.github.io/refs/heads/master/db.json'
+const TARGET = 'README.md';
+
+async function updatePostList() {
+  try {
+    const res = await axios.get(JSON_URL, {
+      verify: false
+    })
+    const posts = res.data?.models?.Post || []
+    // sort by  latest time
+    posts.sort((p1, p2) => {
+      console.log((new Date(p2.date)) - (new Date(p1.date)))
+      return (new Date(p2.date)) - (new Date(p1.date))
+    })
+    // slice the top5
+    let liststr = '';
+    posts.slice(0, 5).forEach(p => {
+      let url = `https://www.jvxiao.cn/posts/${p.slug}.html`;
+      liststr += `- ${p.date.slice(0, 10)} [${p.title}](${url})\n`
+    })
+    liststr += `- [阅读更多...]('https://www.jvxiao.cn/archives/')`;
+    console.log(liststr)
+
+    let readme = readFileSync(TARGET, 'utf-8').toString();
+    readme = readme.replace(/(?<=<!-- blog-start-->)([\s\w\S]*)(?=<!-- blog-end -->)/, `\n${liststr}\n`);
+    writeFileSync(TARGET, readme)
+  }catch(err) {
+    console.log(err)
+  }
+}
+
+updatePostList()
